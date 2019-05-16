0930ae2c95a98fae87cd9c0127b3b5eb8c15ea73
ramiyerk <ramiyerk@gmail.com>
2019-05-16 16:39:51 +0530

    Update Calculator.java

--- src/main/java/com/mahesh/Calculator.java
+++ src/main/java/com/mahesh/Calculator.java
@@ -1,9 +1,10 @@
 package com.mahesh;
 
+}
+
 public class Calculator {
 	 int output;
-	 int input1;
-	 int input2;
+
 		public int addFunction(int num1,int num2) {
 			return output=num1 + num2;
 			
@@ -19,5 +20,5 @@ public class Calculator {
 		public int divideFunction(int num1,int num2) {
 			return output=num1 / num2;
 			
-		}
+		//}
 }
